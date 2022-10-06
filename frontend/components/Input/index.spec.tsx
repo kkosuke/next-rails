e3f328068f6734d14ad90e4cbf6b1353297639b4
kkosuke <kkosuke007@gmail.com>
2022-10-07 01:25:57 +0900

    test

---
+++ frontend/components/Input/index.spec.tsx
@@ -0,0 +1,47 @@
+import {
+  render,
+  screen,
+  RenderResult,
+  fireEvent,
+  getByRole,
+} from '@testing-library/react'
+import { Input } from './index'
+
+describe('Input', () => {
+  let renderResult: RenderResult
+
+  beforeEach(() => {
+    renderResult = render(<Input id="username" label="Username" />)
+  })
+  afterEach(() => {
+    renderResult.unmount()
+  })
+
+  it('should empty in input on initial render', () => {
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    // input の値が空かどうか
+    expect(inputNode).toHaveValue('')
+  })
+
+  it('should show input text', () => {
+    const inputText = 'test input text'
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: inputText } })
+    // input の値が、指定通りあるか
+    expect(inputNode).toHaveValue(inputText)
+  })
+
+  it('should reset when user clicks button', () => {
+    const inputText = 'test input text'
+    const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: inputText } })
+
+    const buttonNode = screen.getByRole('button', {
+      name: 'Reset',
+    }) as HTMLButtonElement
+    fireEvent.click(buttonNode)
+
+    // input の値が、指定通りあるか
+    expect(inputNode).toHaveValue('')
+  })
+})
