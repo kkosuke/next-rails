1fa5ccb18fabca39c806060d57933db360a17e82
kkosuke <kkosuke007@gmail.com>
2022-11-15 22:22:02 +0900

    すこし

---
+++ frontend/src/pages/styled-components/index.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from 'next'
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="styled-components/mixin">mixin</a>
+        </li>
+        <li>
+          <a href="styled-components/props">props</a>
+        </li>
+        <li>
+          <a href="styled-components/span">span</a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Page
